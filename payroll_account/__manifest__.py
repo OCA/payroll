b2b7ac9d26f94307f2bf0ee00ec6ebe4f2e4cc69
aromera <aromera@pmsastro.es>
2022-01-07 09:09:05 +0100

    [FIX] payroll_account: Add taxes to account.move.line

--- payroll_account/__manifest__.py
+++ payroll_account/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Payroll Accounting",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/payroll",
     "license": "LGPL-3",
