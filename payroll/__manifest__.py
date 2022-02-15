cd2810a41528ba2006f8f6233c18bb3a4deb8190
nicolasrsande <nicolasrsande@gmail.com>
2022-02-14 23:59:54 -0300

    [14.0] [IMP] Code fields for python compute fields

--- payroll/__manifest__.py
+++ payroll/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll",
-    "version": "14.0.1.2.0",
+    "version": "14.0.1.3.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/payroll",
     "sequence": 38,
