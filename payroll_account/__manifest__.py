248913d27f2872e8926b3d423436a0419028b43b
nicolasrsande <nicolasrsande@gmail.com>
2022-05-17 00:44:33 -0300

    [IMP] payroll_account: add nimarosa to manteiners

--- payroll_account/__manifest__.py
+++ payroll_account/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Payroll Accounting",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/payroll",
     "license": "LGPL-3",
@@ -10,5 +10,5 @@
     "depends": ["payroll", "account"],
     "data": ["views/hr_payroll_account_views.xml"],
     "demo": ["demo/hr_payroll_account_demo.xml"],
-    "maintainers": ["appstogrow"],
+    "maintainers": ["appstogrow", "nimarosa"],
 }
