f6b38457a795e5b6fe1a851108affffb2c0fcf80
Michael Telahun <mtm@trevi.et>
2022-06-19 21:58:15 +0300

    [IMP] payroll: refactor of local dictionary handling in payslip for salary rule processing
    This should make it easier for other modules to inherit and modify the
    payroll module. This solution allows other modules to arbitrarily
    inject their own values into salary rules. Furthermore, payroll managers
    will be able to continue using '.' (dot) notation to refer to these new
    values. Module implementers can inherit and overload the following methods:
    
    get_payroll_dict()  - for providing values under the generic `payroll.` object
    get_baselocaldict() - for providing 'first-level' objects that are
                          available directly to salary rules. For the sake
                          of clarity the object string should start with
                          the module name. Example: if the module is named
                          payroll_foo the values should be under `payroll_foo.`
    get_contract_dict() - for values that depend on the current contract being evaluated
                          These values will be available under `this_contract.`
    
    In the inherited methods the modules should return a python dictionary of
    discreet values and/or nested objects using a BaseBrowsableObject.
    Example:
    If the module name is my_module:
        res = super().get_baselocaldict(contract, contracts)
        res.update({
            "my_module": BaseBrowsableObject(
                "foo": 10.0,
                "bar": 25.0,
                "baz": BaseBrowsableObject(
                    {
                        value1: 100.0,
                        value2: 150.0,
                    }
                )
            )
        })
        return res
    
    The above values can be accessed in salary rules as:
    my_module.foo
    my_module.bar
    my_module.baz.value1
    my_module.baz.value2

--- payroll/tests/test_payslip_flow.py
+++ payroll/tests/test_payslip_flow.py
@@ -7,6 +7,20 @@ from .common import TestPayslipBase
 
 
 class TestPayslipFlow(TestPayslipBase):
+    def setUp(self):
+        super().setUp()
+
+        self.test_rule = self.SalaryRule.create(
+            {
+                "name": "Test rule",
+                "code": "TEST",
+                "category_id": self.categ_alw.id,
+                "sequence": 5,
+                "amount_select": "code",
+                "amount_python_compute": "result = contract.wage",
+            }
+        )
+
     def test_00_payslip_flow(self):
         """Testing payslip flow and report printing"""
 
@@ -114,4 +128,26 @@ class TestPayslipFlow(TestPayslipBase):
             "action_payslip_lines_contribution_register",
             context=context,
             our_module="payroll",
+        )
+
+    def test_contract_qty(self):
+
+        # I set the test rule to detect contract count
+        self.test_rule.amount_python_compute = (
+            "result = payroll.contracts and payroll.contracts.count or -1.0"
+        )
+        self.developer_pay_structure.write({"rule_ids": [(4, self.test_rule.id)]})
+
+        # I put all eligible contracts (including Richard's) in an "open" state
+        self.apply_contract_cron()
+
+        # I create an employee Payslip and process it
+        richard_payslip = self.Payslip.create({"employee_id": self.richard_emp.id})
+        richard_payslip.onchange_employee()
+        richard_payslip.compute_sheet()
+
+        line = richard_payslip.line_ids.filtered(lambda l: l.code == "TEST")
+        self.assertEqual(len(line), 1, "I found the Test line")
+        self.assertEqual(
+            line[0].amount, 1.0, "The calculated dictionary value 'contracts.qty' is 1"
         )
