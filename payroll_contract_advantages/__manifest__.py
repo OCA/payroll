1d83e0bb37a64fc1d7f3f3e150dee99211ae9006
nicolasrsande <nicolasrsande@gmail.com>
2022-06-21 14:43:44 -0300

    [IMP] payroll: Remove contract_advantage_template model and new module hr_contract_advantages
    [ADD] Add new module hr_contract_advantages
    
    [FIX] payroll_contract_advantages: Fix typo in view
    
    [FIX] payroll_contract_advantages: Fix typo in import
    
    [IMP] payroll: change security grups
    
    [FIX] payroll: fix version
    
    [FIX] payroll_contract_advantages: fix import typo
    
    [FIX] payroll_contract_advantages: Fix security groups
    
    [FIX] payroll_contract_advantages: Fix menu typo
    
    [IMP] payroll_contract_advantages: Fix get_contract_dict function
    
    [UPD] Update payroll_contract_advantages.pot
    
    [UPD] README.rst
    
    [ADD] icon.png
    
    payroll_contract_advantages 14.0.2.0.0

---
+++ payroll_contract_advantages/__manifest__.py
@@ -0,0 +1,19 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Payroll Contract Advantages",
+    "version": "14.0.2.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/payroll",
+    "summary": "Allow to define contract advantages for employees.",
+    "license": "LGPL-3",
+    "author": "Nimarosa, Odoo Community Association (OCA)",
+    "depends": ["hr_contract", "payroll"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_contract_advantage_views.xml",
+        "views/hr_contract_views.xml",
+    ],
+    "application": True,
+    "maintainers": ["nimarosa"],
+}
