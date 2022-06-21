1d4d4f357262ece59a52f8be06f97fa0d98b1408
Michael Telahun <mtm@trevi.et>
2022-06-21 20:05:44 +0300

    [IMP] payroll: add a test to show that if result_* are not set they get default values

--- payroll/tests/test_hr_salary_rule.py
+++ payroll/tests/test_hr_salary_rule.py
@@ -45,3 +45,25 @@ class TestSalaryRule(TestPayslipBase):
         self.assertEqual(line.amount, 0.0, "The amount is zero")
         self.assertEqual(line.rate, 0.0, "The rate is zero")
         self.assertEqual(line.quantity, 0.0, "The quantity is zero")
+
+    def test_python_code_result_not_set(self):
+
+        self.test_rule.amount_python_compute = "result = 2"
+
+        # Open contracts
+        cc = self.env["hr.contract"].search([("employee_id", "=", self.richard_emp.id)])
+        cc.kanban_state = "done"
+        self.env.ref(
+            "hr_contract.ir_cron_data_contract_update_state"
+        ).method_direct_trigger()
+
+        # Create payslip and compute
+        payslip = self.Payslip.create({"employee_id": self.richard_emp.id})
+        payslip.onchange_employee()
+        payslip.compute_sheet()
+
+        line = payslip.line_ids.filtered(lambda l: l.code == "TEST")
+        self.assertEqual(len(line), 1, "I found the Test line")
+        self.assertEqual(line.amount, 2.0, "The amount is zero")
+        self.assertEqual(line.rate, 100.0, "The rate is zero")
+        self.assertEqual(line.quantity, 1.0, "The quantity is zero")
