1febba34e7fa6fc29e90cec3e022354ebc7d2f61
nicolasrsande <nicolasrsande@gmail.com>
2022-06-21 14:43:44 -0300

    [IMP] payroll: Remove contract_advantage_template model and new module hr_contract_advantages
    [ADD] Add new module hr_contract_advantages
    
    [FIX] payroll_contract_advantages: Fix typo in view
    
    [FIX] payroll_contract_advantages: Fix typo in import
    
    [IMP] payroll: change security grups
    
    [FIX] payroll: fix version
    
    [FIX] payroll_contract_advantages: fix import typo
    
    [FIX] payroll_contract_advantages: Fix security groups
    
    [FIX] payroll_contract_advantages: Fix menu typo
    
    [IMP] payroll_contract_advantages: Fix get_contract_dict function

---
+++ payroll_contract_advantages/models/__init__.py
@@ -0,0 +1,6 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import hr_contract_advantage_template
+from . import hr_contract_advantage
+from . import hr_contract
+from . import hr_payslip
