d09e3d47047ae740c6298b2b370df86e294345c7
Michael Telahun <mtm@trevi.et>
2022-08-11 15:44:15 +0300

    [IMP] payroll: allow the use of positive values in leave days/hours calculation
    The system parameter "payroll.leaves_positive" is used to control
    whether the value for number_of_days/number_of_hours is positive or
    negative. When False (the default) it wil be negative.

--- payroll/tests/__init__.py
+++ payroll/tests/__init__.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_browsable_object
+from . import test_hr_payslip_worked_days
 from . import test_hr_salary_rule
 from . import test_payslip_flow
