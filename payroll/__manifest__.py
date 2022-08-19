70435c386765684cd816ec8ff80a14e3079059b0
nicolasrsande <nicolasrsande@gmail.com>
2022-08-19 14:14:34 -0300

    [FIX] payroll: Fix Refactored _compute_name to contemplate more than one record
    [FIX] payroll: fix _compute_name singleton error

--- payroll/__manifest__.py
+++ payroll/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll",
-    "version": "14.0.4.0.0",
+    "version": "14.0.4.0.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/payroll",
     "sequence": 38,
