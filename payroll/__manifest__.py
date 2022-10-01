a60091ba28e3d00c4d4d7c972671696ac41adbb9
nicolasrsande <nicolasrsande@gmail.com>
2022-10-01 03:09:06 -0300

    [14.0] [IMP] payroll: improve salary_rule and categories views and documentation
    [14.0] [IMP] payroll: improve salary rules views and documentation
    
    [14.0] [IMP] payroll: improve salary rules  and categories views
    
    [14.0] [IMP] payroll: improve salary rules views and documentation
    
    [14.0] [IMP] payroll: improve salary rules views and documentation
    
    [IMP] payroll: fix repeated words
    
    [14.0] [IMP] payroll: fix typos

--- payroll/__manifest__.py
+++ payroll/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll",
-    "version": "14.0.5.4.1",
+    "version": "14.0.5.5.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/payroll",
     "sequence": 38,
