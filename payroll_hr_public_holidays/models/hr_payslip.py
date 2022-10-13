846deaf46dcf36e02f538cd341361371ab9a1139
nicolasrsande <nicolasrsande@gmail.com>
2022-10-13 19:11:44 -0300

    [14.0] [ADD] payroll_hr_public_holidays

--- payroll_hr_public_holidays/models/hr_payslip.py
+++ payroll_hr_public_holidays/models/hr_payslip.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2021 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from datetime import datetime, time
 
 from odoo import _, api, models
 
@@ -15,28 +14,29 @@ class HrPayslip(models.Model):
         for contract in contracts.filtered(
             lambda contract: contract.resource_calendar_id
         ):
-            day_from = datetime.combine(date_from, time.min)
-            day_to = datetime.combine(date_to, time.max)
-            day_contract_start = datetime.combine(contract.date_start, time.min)
             # only use payslip day_from if it's greather than contract start date
-            if day_from < day_contract_start:
-                day_from = day_contract_start
+            if date_from < contract.date_start:
+                date_from = contract.date_start
             # == compute public holidays == #
-            pholidays = self._compute_public_holidays_days(contract, day_from, day_to)
+            pholidays = self._compute_public_holidays_days(contract, date_from, date_to)
             if pholidays["number_of_days"] > 0:
                 res.append(pholidays)
         return res
 
-    def _compute_public_holidays_days(self, contract, day_from, day_to):
+    def _compute_public_holidays_days(self, contract, date_from, date_to):
         # get public holidays list
         public_holidays = self.env["hr.holidays.public"].get_holidays_list(
-            year=day_from.year,
-            start_dt=day_from,
-            end_dt=day_to,
+            year=date_from.year,
+            start_dt=date_from,
+            end_dt=date_to,
             employee_id=contract.employee_id.id,
         )
         ph_days = len(public_holidays)
-        ph_hours = ph_days * contract.employee_id.resource_calendar_id.hours_per_day
+        ph_hours = (
+            ph_days * 8
+        )  # Use 8 as default value if employee has no resource_calendar
+        if contract.employee_id.resource_calendar_id:
+            ph_hours = ph_days * contract.employee_id.resource_calendar_id.hours_per_day
         return {
             "name": _("Public Holidays Leaves"),
             "sequence": 10,
