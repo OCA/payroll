a010deb330015c6c69f73bb2387fbb0c42127973
Henrik Norlin <henrik.norl@gmail.com>
2022-10-28 12:23:01 +0200

    [IMP] payroll: hr.salary.rule is readonly for payroll user

--- payroll/__manifest__.py
+++ payroll/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll",
-    "version": "14.0.5.5.2",
+    "version": "14.0.5.5.4",
     "category": "Human Resources",
     "website": "https://github.com/OCA/payroll",
     "sequence": 38,
