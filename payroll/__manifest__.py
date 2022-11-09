61b408844cef97463ccf59a3cd0e64ca593bc2a3
Henrik Norlin <henrik.norl@gmail.com>
2022-11-09 09:04:24 +0100

    [IMP] payroll: test rule and category with and without code

--- payroll/__manifest__.py
+++ payroll/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll",
-    "version": "14.0.6.0.0",
+    "version": "14.0.6.1.0",
     "category": "Payroll",
     "website": "https://github.com/OCA/payroll",
     "sequence": 38,
