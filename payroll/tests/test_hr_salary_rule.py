4feb0fec46ecff5962e36dea79fd519d82490dda
Henrik Norlin <henrik.norl@gmail.com>
2022-11-09 09:04:24 +0100

    [IMP] payroll: test rule and category with and without code

--- payroll/tests/test_hr_salary_rule.py
+++ payroll/tests/test_hr_salary_rule.py
@@ -139,3 +139,58 @@ class TestSalaryRule(TestPayslipBase):
         child_line = payslip.line_ids.filtered(lambda l: l.code == "CHILD_TEST")
         self.assertEqual(len(parent_line), 0, "No parent line found")
         self.assertEqual(len(child_line), 0, "No child line found")
+
+    def test_rule_and_category_with_and_without_code(self):
+        rule_test_code = self.SalaryRule.create(
+            {
+                "name": "rule test code",
+                "code": "rule_test_code",
+                "sequence": 100,
+                "amount_select": "code",
+                "amount_python_compute": "result = categories.BASIC + HRA",
+            }
+        )
+        category_without_code = self.SalaryRuleCateg.create({"name": "categ no code"})
+        rule_without_code = self.SalaryRule.create(
+            {
+                "name": "rule without code",
+                "category_id": category_without_code.id,
+                "sequence": 10,
+                "amount_select": "code",
+                "amount_python_compute": "result = 100",
+            }
+        )
+        rule_without_category = self.SalaryRule.create(
+            {
+                "name": "rule without category",
+                "sequence": 10,
+                "amount_select": "code",
+                "amount_python_compute": "result = 100",
+            }
+        )
+        structure = self.PayrollStructure.create(
+            {
+                "name": "test code and id",
+                "rule_ids": [
+                    (4, self.rule_basic.id),
+                    (4, self.rule_hra.id),
+                    (4, rule_test_code.id),
+                    (4, rule_without_code.id),
+                    (4, rule_without_category.id),
+                ],
+            }
+        )
+        payslip = self.Payslip.create(
+            {
+                "employee_id": self.richard_emp.id,
+                "contract_id": self.richard_contract.id,
+                "struct_id": structure.id,
+            }
+        )
+        payslip.compute_sheet()
+        line = payslip.line_ids.filtered(lambda l: l.code == "rule_test_code")
+        self.assertEqual(line.total, 7000, "5000 categories.BASIC + 2000 HRA = 7000")
+        line = payslip.line_ids.filtered(lambda l: l.name == "rule without code")
+        self.assertEqual(len(line), 1, "Line found: rule without code")
+        line = payslip.line_ids.filtered(lambda l: l.name == "rule without category")
+        self.assertEqual(len(line), 1, "Line found: rule without category")
