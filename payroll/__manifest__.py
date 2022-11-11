db9aab9eb34630ece28996c46acda1c7dc128414
Michael Telahun <mtm@trevi.et>
2022-11-11 14:37:23 +0300

    [MIG] payroll: Migration to 16.0

--- payroll/__manifest__.py
+++ payroll/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Payroll",
     "website": "https://github.com/OCA/payroll",
     "sequence": 38,
