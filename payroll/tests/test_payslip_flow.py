e87bd6ca146d0bdfa16080ad613d001015c0c89d
Michael Telahun <mtm@trevi.et>
2022-11-11 14:37:23 +0300

    [MIG] payroll: Migration to 16.0

--- payroll/tests/test_payslip_flow.py
+++ payroll/tests/test_payslip_flow.py
@@ -144,12 +144,16 @@ class TestPayslipFlow(TestPayslipBase):
         # I print the payslip report
         data, data_format = self.env.ref(
             "payroll.action_report_payslip"
-        )._render_qweb_pdf(richard_payslip.ids)
+        )._render_qweb_pdf(
+            self.env.ref("payroll.action_report_payslip"), res_ids=richard_payslip.ids
+        )
 
         # I print the payslip details report
         data, data_format = self.env.ref(
             "payroll.payslip_details_report"
-        )._render_qweb_pdf(richard_payslip.ids)
+        )._render_qweb_pdf(
+            self.env.ref("payroll.payslip_details_report"), res_ids=richard_payslip.ids
+        )
 
         # I print the contribution register report
         context = {
