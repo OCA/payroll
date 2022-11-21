28f60af41984f4b748a4feb7e2bdfe4682b2753f
nicolasrsande <nicolasrsande@gmail.com>
2022-11-20 22:06:06 -0300

    [14.0] [IMP] payroll_rule_time_parameter: Improve rule_porameter function

--- payroll_rule_time_parameter/models/hr_payslip.py
+++ payroll_rule_time_parameter/models/hr_payslip.py
@@ -7,11 +7,9 @@ from odoo import models
 class HrPayslip(models.Model):
     _inherit = "hr.payslip"
 
-    def rule_parameter(self, code, date=False, return_type="float"):
+    def rule_parameter(self, code, date=False, data_type="value"):
         self.ensure_one()
         if not date:
             date = self.date_from
-        time_parameter = self.get_time_parameter(code, date=date)
-        return (
-            return_type == "float" and float(time_parameter) or time_parameter or 0.00
-        )
+        time_parameter = self.get_time_parameter(code, date=date, data_type=data_type)
+        return time_parameter
