cd9654770d317585e80f0543b2a9748a25bbdb87
FernandoRomera <fromera@astrohotel.es>
2023-03-28 10:14:38 +0200

    [14.0][FIX] payroll_account: Fix multicompanies and refactoring

--- payroll_account/models/hr_salary_rule.py
+++ payroll_account/models/hr_salary_rule.py
@@ -1,10 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
 
 from odoo import fields, models
-
-logger = logging.getLogger(__name__)
 
 
 class HrSalaryRule(models.Model):
