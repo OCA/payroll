2a5960ae9323dc6316d8b4f0babf2eb000b72c77
Henrik Norlin <henrik.norl@gmail.com>
2024-02-15 22:52:51 +0100

    [ADD] payroll_fetch_prev_month

---
+++ payroll_fetch_prev_month/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Payroll fetch previous month",
+    "summary": "Option to fetch worked days / leaves from the previous month",
+    "author": "Henrik Norlin, Odoo Community Association (OCA)",
+    "category": "Payroll",
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "depends": [
+        "payroll",
+    ],
+    "license": "AGPL-3",
+    "maintainers": ["norlinhenrik"],
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/payroll",
+}
---
+++ payroll_fetch_prev_month/models/__init__.py
@@ -0,0 +1,2 @@
+from . import hr_payslip
+from . import res_config_settings
