6a17f30cdabffe734b6b1691d78fa298a5dc956c
FernandoRomera <fromera@astrohotel.es>
2025-08-14 09:39:41 +0200

    [18.0][FIX] payroll_account: resilience in pre-migration script

--- payroll_account/migrations/18.0.1.0.2/pre-migration.py
+++ payroll_account/migrations/18.0.1.0.2/pre-migration.py
@@ -1,11 +1,18 @@
 from openupgradelib import openupgrade
 
-field_names = ["account_debit", "account_credit"]
-
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_columns(
-        env.cr,
-        {"hr_salary_rule": [(field_name, None) for field_name in field_names]},
-    )
+    # in v13, these fields were not company_dependent
+    if openupgrade.column_exists(env.cr, "hr_salary_rule", "account_debit"):
+        field_names = ["account_debit"]
+        if openupgrade.column_exists(env.cr, "hr_salary_rule", "account_credit"):
+            field_names += ["account_credit"]
+        openupgrade.rename_columns(
+            env.cr,
+            {"hr_salary_rule": [(field_name, None) for field_name in field_names]},
+        )
+        openupgrade.rename_columns(
+            env.cr,
+            {"hr_payslip_line": [(field_name, None) for field_name in field_names]},
+        )
