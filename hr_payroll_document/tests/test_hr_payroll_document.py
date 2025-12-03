43638eee321a1ffd620ceed1a0552e00ca479daf
Simone Rubino <simone.rubino@pytech.it>
2025-12-03 11:20:39 +0100

    [REF] hr_payroll_document: Allow to use fallback reader
    Also refactor tests to have all the needed setup in the same place

--- hr_payroll_document/tests/test_hr_payroll_document.py
+++ hr_payroll_document/tests/test_hr_payroll_document.py
@@ -9,11 +9,6 @@ from odoo.addons.hr_payroll_document.tests.common import TestHrPayrollDocument
 
 
 class TestHRPayrollDocument(TestHrPayrollDocument):
-    def fill_company_id(self):
-        self.env.company.country_id = self.env["res.country"].search(
-            [("name", "=", "Spain")]
-        )
-
     def test_extension_error(self):
         self.wizard = self._create_wizard(
             "January", "hr_payroll_document/tests/test.docx"
