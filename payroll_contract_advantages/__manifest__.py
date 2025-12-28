ef3cce98f61d9baeacbb9bf7b47c34b44e2522e2
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-12-28 10:30:11 -0300

    [MIG] payroll_contract_advantages: Migration to 18.0

--- payroll_contract_advantages/__manifest__.py
+++ payroll_contract_advantages/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payroll Contract Advantages",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Payroll",
     "website": "https://github.com/OCA/payroll",
     "summary": "Allow to define contract advantages for employees.",
