d2d0219a5614aafa36a84bc069911cb5b3e3107d
Simone Rubino <simone.rubino@pytech.it>
2025-12-31 12:14:54 +0100

    [IMP] hr_payroll_document: Optional payroll encryption

--- hr_payroll_document/models/res_users.py
+++ hr_payroll_document/models/res_users.py
@@ -1,8 +1,21 @@
-from odoo import models
+# Copyright 2025 Simone Rubino - PyTech
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+from odoo.addons.hr.models.res_users import HR_WRITABLE_FIELDS
+
+HR_WRITABLE_FIELDS.append("no_payroll_encryption")
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
+
+    no_payroll_encryption = fields.Boolean(
+        related="employee_id.no_payroll_encryption",
+        readonly=False,
+        related_sudo=False,
+    )
 
     def action_get_attachment_tree_view(self):
         action = self.env.ref("base.action_attachment").sudo().read()[0]
--- hr_payroll_document/wizard/payroll_management_wizard.py
+++ hr_payroll_document/wizard/payroll_management_wizard.py
@@ -50,9 +50,10 @@ class PayrollManagamentWizard(models.TransientModel):
 
             path = "/tmp/" + _("Payroll ") + employee.name + ".pdf"
 
-            # Encrypt the payroll file with the identification identifier
-            # of the employee
-            pdfWriter.encrypt(employee.identification_id, algorithm="AES-256")
+            if not employee.no_payroll_encryption:
+                # Encrypt the payroll file
+                # with the identification identifier of the employee
+                pdfWriter.encrypt(employee.identification_id, algorithm="AES-256")
 
             f = open(path, "wb")
             pdfWriter.write(f)
