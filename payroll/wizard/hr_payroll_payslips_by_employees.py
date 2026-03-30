2ae9539727dca9533a7ffe631478a106995bd827
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2026-03-30 14:17:22 -0300

    [IMP] payroll: action_clear_employees

--- payroll/wizard/hr_payroll_payslips_by_employees.py
+++ payroll/wizard/hr_payroll_payslips_by_employees.py
@@ -6,9 +6,47 @@ class HrPayslipEmployees(models.TransientModel):
     _name = "hr.payslip.employees"
     _description = "Generate payslips for all selected employees"
 
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        default=lambda self: self.env.company,
+    )
+    department_id = fields.Many2one("hr.department", string="Department")
+    job_id = fields.Many2one("hr.job", string="Job Position")
     employee_ids = fields.Many2many(
         "hr.employee", "hr_employee_group_rel", "payslip_id", "employee_id", "Employees"
     )
+
+    def _get_reopen_wizard_action(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": self._name,
+            "res_id": self.id,
+            "view_mode": "form",
+            "target": "new",
+            "context": dict(self.env.context),
+        }
+
+    def action_apply_employee_filter(self):
+        self.ensure_one()
+        domain = []
+        if self.company_id:
+            domain.append(("company_id", "=", self.company_id.id))
+        if self.department_id:
+            domain.append(("department_id", "=", self.department_id.id))
+        if self.job_id:
+            domain.append(("job_id", "=", self.job_id.id))
+        employee_ids = self.env["hr.employee"].search(domain).ids
+        self.write({"employee_ids": [(6, 0, employee_ids)]})
+        return self._get_reopen_wizard_action()
+
+    def action_clear_employees(self):
+        self.ensure_one()
+        self.write({"employee_ids": [(5, 0, 0)]})
+        # Preserve originating action context (active_id on hr.payslip.run). A dict
+        # return is required: other truthy values become act_window_close in web.
+        return self._get_reopen_wizard_action()
 
     def compute_sheet(self):
         payslips = self.env["hr.payslip"]
