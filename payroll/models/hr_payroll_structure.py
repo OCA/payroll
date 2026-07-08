d4b05af5301a3bff493f1ff967342a7ccae189a0
stferraro <gerferr83@gmail.com>
2026-07-08 15:33:56 -0400

    [19.0][IMP] REMOVE company_id required=True

--- payroll/models/hr_payroll_structure.py
+++ payroll/models/hr_payroll_structure.py
@@ -24,7 +24,6 @@ class HrPayrollStructure(models.Model):
     company_id = fields.Many2one(
         "res.company",
         string="Company",
-        required=True,
         copy=False,
         default=lambda self: self.env.company,
     )
